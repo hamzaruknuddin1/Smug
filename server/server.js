2d7b9d56015b42eedaf1a40182424ef3ee8c27fc
TIMEXPERTS\rhamza <rhamza@workforcesoftware.com>
2023-03-31 17:02:43 +0500

    adding tax details to prompt of system 2

--- server/server.js
+++ server/server.js
@@ -189,7 +189,7 @@ app.post('/', async (req, res) => {
             model: 'gpt-3.5-turbo',
             messages: messages,
             temperature: 1,
-            max_tokens: 3000,
+            max_tokens: 10000,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0
