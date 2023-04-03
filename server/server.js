59f7925f151b8ba80165a6597ddc0715b21e6466
TIMEXPERTS\rhamza <rhamza@workforcesoftware.com>
2023-04-03 15:47:22 +0500

    just smaller version of faqs

--- server/server.js
+++ server/server.js
@@ -18,45 +18,6 @@ const messages = [{"role": "system", "content": `you are a smart US company form
     What type of insurance do I need for a TCP License?
     A Public Liability and Property Damage insurance policy is the main insurance policy you are required to have coverage for on your vehicles. The minimum coverage starts at $750,000 for a seven-seater vehicle, and increases with larger seating capacity.
     
-    What is Commercial Registration?
-    Commercial registration classifies a vehicle to be used primarily for business purposes. This classification signifies to the DMV that this vehicle will no longer be considered for personal use, but instead for commercial use, regardless of who the registered owner of the vehicle is.
-    
-    Who needs a Drug Test for a TCP License?
-    It is required by federal law that all drivers operating under a TCP license comply with a drug and alcohol screening prior to obtaining a TCP license. Owners who will not be operating the vehicles, but will employ drivers to do so, are required to enroll their drivers in drug and alcohol testing services and ensure the completion of any pre-employment, post-accident, or random drug and alcohol screening requirements.
-    
-    Can I drive if I have a medical marijuana card?
-    Marijuana use, even with a medical marijuana card, is not permitted while operating passenger transportation businesses. CPUC follows the Code of Federal Regulations 49 Part A which states that any positive result of marijuana, cocaine, amphetamines, phencyclidine (PCP), or opioid specimen will be rejected.
-    
-    Can I have Drivers as Independent Contractors?
-    CPUC does not allow for independent contract work regarding any vehicle listed in a TCP fleet. This means that it is mandatory for an owner to classify anyone operating his or her vehicles as a driver for their business. Any driver who is not an owner of the business is considered an employee.
-    
-    What is the difference between the TCP licenses?
-    There are a total of six different TCP licenses offered by the CPUC. Each license varies in restrictions and flexibility relating to permitted business operations under that license type. The restrictions focus on mileage, seating capacity, business activity, and whether or not the license is transferable.
-    
-    What is the Difference Between the TCP B permit and P Permit?
-    License type B and P are the most common for strictly passenger pick-up and drop-off transportation activities. The main difference of license type B is that it provides flexibility on vehicle seating capacity, allowing you to operate vehicles with any number of passengers within a 125-mile radius. Whereas license type P provides flexibility on mileage, allowing you to operate vehicles with less than sixteen passengers from any point to any point within the state of California. Another important difference to note is B permit is transferable, while P permit is not.
-    
-    Why is the TCP A Permit Preferred?
-    While TCP type A is the most expensive, it is the most preferable due providing the best flexibility in all areas of operations. With type A, an operator can utilize vehicles with any seating capacity, operate from any point to any point within the state of California, conduct activity beyond passenger pick-up and drop-off, and is transferable.
-    
-    Do I need a Corporation or LLC to operate a TCP License?
-    You do not need a corporation or LLC to apply for a TCP license. CPUC allows an applicant to obtain a TCP license as an individual, or sole proprietorship.
-    
-    Why is a Corporation or LLC preferred when starting a TCP License?
-    Obtaining a TCP license for a Corporation or LLC ensures substantial protection, greater flexibility for changes, and high-reward for business owners. These entity types can deduct more expenses, transfer licenses, add or remove partners freely, and within two years of operation, finance vehicles and achieve lines of credit.
-    
-    How much does it cost to Obtain a TCP License?
-    In addition to BusinessRocket’s service fee of $695, there is an application fee of either $1,000 or $1,500, depending on the type of license you apply for. The additional fee gets paid directly to the CPUC at the time of submitting the application.
-    
-    Can I lease a car for my TCP License?
-    Yes, you may utilize a leased vehicle for your TCP operations, so long as you have adequate lease agreement and the vehicle is insured under your company’s name, not the company you are leasing from. In order for a lease agreement to be adequate, the lease must be commercial, not personal.
-    
-    What if my finance company says no to transferring my Registration to Commercial Use?
-    The CPUC does not have any Year, Make or Model requirements for a vehicle being added to the TCP license. However, if you plan to provide transportation services, the vehicle must be operational and registered for commercial use. It is also recommended you verify with your insurance broker and any potential vendors, like Uber or Lyft, any specified vehicle requirements.
-    
-    Do I need a USDOT or CA number for my TCP License?
-    A USDOT and CA number are only required in three situations. 1) Your vehicle is 10 passengers or more, 2) your vehicle is modified or stretched to accommodate more seats, or 3) you will be operating across state lines, also known as interstate travel.
-    
     ) `}];
 
 dotenv.config()
